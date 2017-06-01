376ad9c6fa858e1fda121ffd52fead7699734441
Shohei Kajihara <s.white.capybara@gmail.com>
2017-06-01 10:50:06 +0900

    [from now] 2017/06/01 10:50:06
    diff --git a/app/controllers/users_controller.rb b/app/controllers/users_controller.rb
    index aa13a01..8d70839 100644
    --- a/app/controllers/users_controller.rb
    +++ b/app/controllers/users_controller.rb
    @@ -25,7 +25,8 @@ class UsersController < ApplicationController
       def update
         @user = User.find(params[:id])
         if @user.update_attributes(user_params)
    -    # TODO:
    +      flash[:success] = "Profile updated"
    +      redirect_to @user
         else
           render 'edit'
         end
    diff --git a/app/models/user.rb b/app/models/user.rb
    index 7f08425..0880354 100644
    --- a/app/models/user.rb
    +++ b/app/models/user.rb
    @@ -13,7 +13,7 @@ class User < ApplicationRecord
                 uniqueness: { case_sensitive: false }
    
       has_secure_password
    -  validates :password, presence: true, length: { minimum: 6 }
    +  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
    
       def remember
         @remember_token = User.new_token
    diff --git a/test/controllers/users_controller_test.rb b/test/controllers/users_controller_test.rb
    index f9fef98..7ec8930 100644
    --- a/test/controllers/users_controller_test.rb
    +++ b/test/controllers/users_controller_test.rb
    @@ -1,9 +1,17 @@
     require 'test_helper'
    
     class UsersControllerTest < ActionDispatch::IntegrationTest
    -  test "should get new" do
    +  def setup
    +    @user = users(:michael)
    +  end
    +
    +  test 'should get new' do
         get signup_path
         assert_response :success
       end
    
    +  test 'should get edit' do
    +    get edit_user_path(@user)
    +    assert_response :success
    +  end
     end

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+  def setup
+    @user = users(:michael)
+  end
+
+  test 'should get new' do
     get signup_path
     assert_response :success
   end
 
+  test 'should get edit' do
+    get edit_user_path(@user)
+    assert_response :success
+  end
 end
