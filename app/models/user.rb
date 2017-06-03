56ff197bcddf6ba817744f7aacbc67030e842104
Shohei Kajihara <s.white.capybara@gmail.com>
2017-06-03 15:57:05 +0900

    Add AccountActivations resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
-  before_save { email.downcase! }
+  before_save :downcase_email
+  before_create :create_activation_digest
 
   validates :name, presence: true, length: { maximum: 50 }
 
@@ -42,5 +43,16 @@ class User < ApplicationRecord
     def new_token
       SecureRandom.urlsafe_base64
     end
+  end
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,9 @@ User.create!(
   email:                 'example@railstutorial.org',
   password:              'foobar',
   password_confirmation: 'foobar',
-  admin:                 true
+  admin:                 true,
+  activated:             true,
+  activated_at:          Time.zone.now
 )
 
 99.times do |n|
@@ -19,9 +21,11 @@ User.create!(
   email = "example-#{n + 1}@railstutorial.org"
   password = 'password'
   User.create!(
-    name: name,
-    email: email,
-    password: password,
-    password_confirmation: password
+    name:                  name,
+    email:                 email,
+    password:              password,
+    password_confirmation: password,
+    activated:             true,
+    activated_at:          Time.zone.now
   )
 end
