483c0a03859e118ca731dedec6814b88f6f1cdc9
Shohei Kajihara <s.white.capybara@gmail.com>
2017-06-18 13:22:17 +0900

    Add sec 12.2, 12.3

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,6 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users
-  resources :account_activations, only: [:edit]
-  resources :password_resets, only: [:new, :create, :edit, :delete]
+  resources :account_activations, only: %i[edit]
+  resources :password_resets, only: %i[new create edit update]
 end
