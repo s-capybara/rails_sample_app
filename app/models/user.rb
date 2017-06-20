4c42f3fe0b1332e0ee1728fab02c387deff928dc
Shohei Kajihara <s.white.capybara@gmail.com>
2017-06-20 11:47:28 +0900

    Add sec 14.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -69,7 +69,9 @@ class User < ApplicationRecord
   end
 
   def feed
-    Micropost.where('user_id = ?', id)
+    Micropost.where('user_id IN (:following_ids) OR user_id = :user_id',
+                    following_ids: following_ids,
+                    user_id: id)
   end
 
   def follow(other_user)
